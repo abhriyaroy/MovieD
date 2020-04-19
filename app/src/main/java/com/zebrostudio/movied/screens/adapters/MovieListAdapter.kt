b7bcdd2cb7213de1d2139efd22a151154b536df9
Abhriya Roy <roy.abhriya@gmail.com>
2020-04-19 10:01:03 +0530

    fix: remove api kep

--- app/src/main/java/com/zebrostudio/movied/screens/adapters/MovieListAdapter.kt
+++ app/src/main/java/com/zebrostudio/movied/screens/adapters/MovieListAdapter.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movied.R
 import com.zebrostudio.movied.repositories.models.MovieItemModel
 import com.zebrostudio.movied.utils.ImageLoader
+import com.zebrostudio.movied.utils.showAnimation
 
 class MovieListAdapter(private val imageLoader: ImageLoader) :
     RecyclerView.Adapter<ViewHolder>() {
@@ -34,6 +37,7 @@ class MovieListAdapter(private val imageLoader: ImageLoader) :
         moviesList[position].let {
             holder.showImage(it.posterUrl)
             holder.showMovieTitle(it.title)
+            holder.attachClickListener()
         }
     }
 
@@ -56,6 +60,12 @@ class ViewHolder(
 
     fun showMovieTitle(title: String) {
         itemView.findViewById<TextView>(R.id.movieTitle).text = title
+    }
+
+    fun attachClickListener() {
+        itemView.findViewById<CardView>(R.id.movieCard).setOnClickListener {
+            itemView.findViewById<ImageView>(R.id.moviePoster).showAnimation(R.anim.shrink_fade_out)
+        }
     }
 
 }
